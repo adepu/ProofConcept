2c16899f6e242f071828bae37c43aaaefde1ea2e
Scott Swanson <sms2fx@captechventures.com>
2014-06-23 13:35:12 -0400

    changes

--- client/angular/controller/controllers.js
+++ client/angular/controller/controllers.js
@@ -11,13 +11,22 @@ app.controller('scraperController', function($scope, Restangular, $filter){
 				angular.forEach(restaurants, function(restaurant, key){
 					
 					setTimeout(function() {
+						// var hours = restaurant.hours.split('\n');
 						var address = restaurant.contact.split('\u2022')[0];
+						var phone = restaurant.contact.split('\u2022')[1];
+						var burgerName = restaurant.burger.split(":")[0];
+						var burgerDescription = restaurant.burger.split(":")[1];
 						Restangular.oneUrl('maps', 'https://maps.googleapis.com/maps/api/geocode/json?address='+address+',+Richmond,+VA').get().then(function(result){
 								var to   = new google.maps.LatLng(result.results[0].geometry.location.lat, result.results[0].geometry.location.lng);
 								var dist = google.maps.geometry.spherical.computeDistanceBetween(from, to) * 0.000621371;
 										console.log(dist);
-								restaurant.distance = dist;
+								restaurant.distance = Math.round(dist * 100) / 100;
 						});
+						// restaurant.hours = hours;
+						restaurant.address = address;
+						restaurant.phone = phone;
+						restaurant.burgerName = burgerName;
+						restaurant.burgerDescription = burgerDescription;
 					}, 100);
 
 				});
