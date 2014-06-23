4ce4835ce969acac2e9cecf086953b738aed13f1
adepu <adepu.shashank@gmail.com>
2014-06-23 13:00:47 -0400

    proof

--- client/angular/controller/controllers.js
+++ client/angular/controller/controllers.js
@@ -1,28 +1,48 @@
-app.controller('scraperController', function($scope, Restangular, $filter){
+app.controller('scraperController', function($scope, Restangular, $filter, $q){
 	function success(position) { 
-		//console.log(position);
 			// stackoverflow: distance between 2 geo locations
 			var from = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+			var mapOptions = {zoom: 11, center: from}
+			var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+			var deferred = $q.defer();
+			var promise = deferred.promise;
+			var orderBy = $filter('orderBy');
+			promise.then(function(restaurants){
+				$scope.restaurants = restaurants;
+				var topRestaurants = orderBy($scope.restaurants, 'distance', false);
+				angular.forEach(topRestaurants, function(restaurant, key){
+					if(key < 5){
+						var marker = new google.maps.Marker({
+								position: new google.maps.LatLng(restaurant.lat,restaurant.lng),
+								map: map,
+								title: 'restaurant.name'
+						});
+					}
+				})
+			});
 
 			Restangular.all('/localhost:8081/scrape').getList().then(function(result) {
 				var restaurants = result;
 				var ret = [];
 				var log = [];
-				angular.forEach(restaurants, function(restaurant, key){
-					
+				deferred.resolve(angular.forEach(restaurants, function(restaurant, key){
+
 					setTimeout(function() {
 						var address = restaurant.contact.split('\u2022')[0];
 						Restangular.oneUrl('maps', 'https://maps.googleapis.com/maps/api/geocode/json?address='+address+',+Richmond,+VA').get().then(function(result){
-								var to   = new google.maps.LatLng(result.results[0].geometry.location.lat, result.results[0].geometry.location.lng);
+								var lat = result.results[0].geometry.location.lat;
+								var lng = result.results[0].geometry.location.lng;
+								var to   = new google.maps.LatLng(lat,lng);
 								var dist = google.maps.geometry.spherical.computeDistanceBetween(from, to) * 0.000621371;
-										console.log(dist);
+										//console.log(dist);
 								restaurant.distance = dist;
+								restaurant.lat = lat;
+								restaurant.lng = lng;
 						});
 					}, 100);
+				}))
 
-				});
-				
-				$scope.restaurants = restaurants;
+				//$scope.restaurants = restaurants;
 			});
 		}
 		function error(msg) {
@@ -35,6 +55,5 @@ app.controller('scraperController', function($scope, Restangular, $filter){
 		}
 
 
-	})
-
-
+	
+})
